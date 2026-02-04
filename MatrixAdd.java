0dea87db99ec5432632a023277949468622028b8
preejith007 <chn25mca020@ceconline.edu>
2026-02-04 10:53:48 +0530

    Add files via upload

---
+++ MatrixAdd.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class MatrixAdd {
+    public static void main(String[] args)
+    {
+       Scanner sc= new Scanner(System.in);
+       System.out.print("Enter rows:");
+       int r= sc.nextInt();
+       System.out.print("Enter columns:");
+       int c= sc.nextInt();
+       int A[][] = new int[r][c];
+       int B[][] = new int[r][c];
+       int C[][] = new int[r][c];
+       System.out.println("Enter matrix A:");
+       for(int i=0;i<r;i++) {
+          for(int j=0;j<c;j++) {
+            A[i][j] = sc.nextInt();
+          }
+       }
+       System.out.println("Enter matrix B:");
+       for(int i=0;i<r;i++) {
+          for(int j=0;j<c;j++) {
+            B[i][j] = sc.nextInt();
+          }
+       }
+       
+       for(int i=0;i<r;i++) {
+          for(int j=0;j<c;j++) {
+            C[i][j]=A[i][j]+B[i][j];
+          }
+       }
+       System.out.println("Sum of Matrix:");
+       for(int i=0;i<r;i++) {
+          for(int j=0;j<c;j++) {
+            System.out.print(C[i][j] +" ");
+          }
+          System.out.println();
+        }
+    }
+ } 
